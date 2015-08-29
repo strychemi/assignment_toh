14d97936a6f112abc8d9606264bbbd2b83fa0a3b
Andrew Baik <and.baik@gmail.com>
2015-08-28 20:28:48 -0400

    made class with basic methods

---
+++ towers.rb
@@ -0,0 +1,17 @@
+class TowerOfHanoi
+
+  def initialize(num_disks)
+    @disk = num_disks
+  end
+
+  def play
+    #Prints intro
+    puts "Welcome to Tower of Hanoi!"
+    puts "Instructions:"
+    puts "Enter where you'd like to move from and to"
+    puts "in the format [1,3]. Type 'quit' to end."
+
+  end
+
+
+end
